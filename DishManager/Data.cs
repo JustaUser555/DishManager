b711d14448089145f2a27b8bb6e9c7da14ce3700
Gabriel Mirandola <mirandola.gabriel07@gmail.com>
2024-07-10 22:06:20 +0200

    Add project files.

---
+++ DishManager/Data.cs
@@ -0,0 +1,54 @@
+﻿namespace DishManagerLibrary
+{
+    public class Dish
+    {
+        public static List<Dish> AllDishes { get; } = new List<Dish>();
+        public string Name { get; set; }
+        public string Recipe { get; set; }
+        public List<Ingredient> Dependencies { get; set; } = new List<Ingredient>();
+
+        public Dish(string name, string recipe)
+        {
+            Name = name;
+            Recipe = recipe;
+            AllDishes.Add(this);
+        }
+    }
+
+    public class Ingredient
+    {
+        public static List<Ingredient> AllIngredients { get; } = new List<Ingredient>();
+        public string Name { get; set; }
+
+        public Ingredient(string name)
+        {
+            Name = name;
+            AllIngredients.Add(this);
+        }
+    }
+
+    public static class KitchenManager
+    {
+        public static void PrintAllDishes()
+        {
+            foreach (var dish in Dish.AllDishes)
+            {
+                Console.WriteLine($"{dish.Name}: {dish.Recipe}");
+                Console.WriteLine("Ingredients:");
+                foreach (var ingredient in dish.Dependencies)
+                {
+                    Console.WriteLine($"- {ingredient.Name}");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public static void PrintAllIngredients()
+        {
+            foreach (var ingredient in Ingredient.AllIngredients)
+            {
+                Console.WriteLine(ingredient.Name);
+            }
+        }
+    }
+}
---
+++ DishManagerWinForms/Form1.cs
@@ -0,0 +1,10 @@
+namespace DishManagerWinForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
